e286ab218b024d6818c7405ff4016368e9d2b8d8
Alexander Baum <baum_alex@gmx.net>
2016-09-30 09:52:42 +0200

    new interval

--- testData.js
+++ testData.js
@@ -4,33 +4,35 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendfile('./public/index.html');
+app.get('/', function (req, res) {
+    res.sendfile('./public/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  socket.on('status', function(msg){
-    if(msg === 'ready'){
-      console.log("streaming started");
-      setInterval(function(){
-        var random = Math.floor(Math.random()*80)+10;
-        socket.emit("test", random);
-        socket.emit("gauge", random);
-      }, 1000);
-    }
-  });
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+    socket.on('status', function (msg) {
+        if (msg === 'ready') {
+            console.log("streaming started");
+
+            function send() {
+                var random = Math.floor(Math.random() * 80) + 10;
+                socket.emit("test", random);
+                socket.emit("gauge", random);
+                var timeout = Math.floor(Math.random() * 4000) + 1000;
+                setTimeout(function () {
+                    send();
+                }, timeout)
+            }
+            send();
 
 
-  /*setInterval(function(){
-    var random = Math.floor(Math.random()*80)+20;
-    socket.broadcast.emit("gauge", random);
-  }, 2000);*/
+        }
+    });
 });
 
-http.listen(3001, function(){
-  console.log('listening on *:3001');
+http.listen(3001, function () {
+    console.log('listening on *:3001');
 });
