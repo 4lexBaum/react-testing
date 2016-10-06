308cc2c097583cd29262209cdebcba3e308d70b0
Gedeon Moritz <gedeon.moritz@sap.com>
2016-10-06 09:57:55 +0200

    borders and colors array

--- src/App.js
+++ src/App.js
@@ -34,40 +34,40 @@ export class App extends React.Component {
                   </Row>
                   <Row>
                     <Col md={3}>
-                      <Tile containerId="drillingTile" streamId="drill" icon="https://image.freepik.com/free-icon/pneumatic-road-drill_318-34720.png" value="100" unit="°C" title="Drilling Temperature" status="stopped"></Tile>
+                      <Tile containerId="drillingTile" streamId="drill" borders="150,200,250,300" colors="#1e90ff,#00bfff,#60B044,#F6C600,#FF0000" icon="https://image.freepik.com/free-icon/pneumatic-road-drill_318-34720.png" value="100" unit="°C" title="Drilling Temperature" status="stopped"></Tile>
                     </Col>
                     <Col md={3}>
-                      <Tile containerId="millingTile" streamId="mill" icon="https://d30y9cdsu7xlg0.cloudfront.net/png/170362-200.png" value="100" unit="°C" title="Milling Temperature" status="stopped"></Tile>
+                      <Tile containerId="millingTile" streamId="mill" borders="150,200,250,300" colors="#1e90ff,#00bfff,#60B044,#F6C600,#FF0000" icon="https://d30y9cdsu7xlg0.cloudfront.net/png/170362-200.png" value="100" unit="°C" title="Milling Temperature" status="stopped"></Tile>
                     </Col>
                     <Col md={3}>
-                      <Tile containerId="drillingTile2" streamId="drill" icon="https://image.freepik.com/free-icon/pneumatic-road-drill_318-34720.png" value="100" unit="°C" title="Drilling Temperature" status="stopped"></Tile>
+                      <Tile containerId="drillingTile2" streamId="drill" borders="150,200,250,300" colors="#1e90ff,#00bfff,#60B044,#F6C600,#FF0000" icon="https://image.freepik.com/free-icon/pneumatic-road-drill_318-34720.png" value="100" unit="°C" title="Drilling Temperature" status="stopped"></Tile>
                     </Col>
                     <Col md={3}>
-                      <Tile containerId="millingTile2" streamId="mill" icon="https://d30y9cdsu7xlg0.cloudfront.net/png/170362-200.png" value="100" unit="°C" title="Milling Temperature" status="stopped"></Tile>
+                      <Tile containerId="millingTile2" streamId="mill" borders="150,200,250,300" colors="#1e90ff,#00bfff,#60B044,#F6C600,#FF0000" icon="https://d30y9cdsu7xlg0.cloudfront.net/png/170362-200.png" value="100" unit="°C" title="Milling Temperature" status="stopped"></Tile>
                     </Col>
                     <Col md={3}>
-                      <Tile containerId="drillingTile3" streamId="drill" icon="https://image.freepik.com/free-icon/pneumatic-road-drill_318-34720.png" value="100" unit="°C" title="Drilling Temperature" status="stopped"></Tile>
+                      <Tile containerId="drillingTile3" streamId="drill" borders="150,200,250,300" colors="#1e90ff,#00bfff,#60B044,#F6C600,#FF0000" icon="https://image.freepik.com/free-icon/pneumatic-road-drill_318-34720.png" value="100" unit="°C" title="Drilling Temperature" status="stopped"></Tile>
                     </Col>
                     <Col md={3}>
-                      <Tile containerId="millingTile3" streamId="mill" icon="https://d30y9cdsu7xlg0.cloudfront.net/png/170362-200.png" value="100" unit="°C" title="Milling Temperature" status="stopped"></Tile>
+                      <Tile containerId="millingTile3" streamId="mill" borders="150,200,250,300" colors="#1e90ff,#00bfff,#60B044,#F6C600,#FF0000" icon="https://d30y9cdsu7xlg0.cloudfront.net/png/170362-200.png" value="100" unit="°C" title="Milling Temperature" status="stopped"></Tile>
                     </Col>
                     <Col md={3}>
-                      <Tile containerId="drillingTile4" streamId="drill" icon="https://image.freepik.com/free-icon/pneumatic-road-drill_318-34720.png" value="100" unit="°C" title="Drilling Temperature" status="stopped"></Tile>
+                      <Tile containerId="drillingTile4" streamId="drill" borders="150,200,250,300" colors="#1e90ff,#00bfff,#60B044,#F6C600,#FF0000" icon="https://image.freepik.com/free-icon/pneumatic-road-drill_318-34720.png" value="100" unit="°C" title="Drilling Temperature" status="stopped"></Tile>
                     </Col>
                     <Col md={3}>
-                      <Tile containerId="millingTile4" streamId="mill" icon="https://d30y9cdsu7xlg0.cloudfront.net/png/170362-200.png" value="100" unit="°C" title="Milling Temperature" status="stopped"></Tile>
+                      <Tile containerId="millingTile4" streamId="mill" borders="150,200,250,300" colors="#1e90ff,#00bfff,#60B044,#F6C600,#FF0000" icon="https://d30y9cdsu7xlg0.cloudfront.net/png/170362-200.png" value="100" unit="°C" title="Milling Temperature" status="stopped"></Tile>
                     </Col>
                     <Col md={3}>
-                      <Tile containerId="drillingTile5" streamId="drill" icon="https://image.freepik.com/free-icon/pneumatic-road-drill_318-34720.png" value="100" unit="°C" title="Drilling Temperature" status="stopped"></Tile>
+                      <Tile containerId="drillingTile5" streamId="drill" borders="150,200,250,300" colors="#1e90ff,#00bfff,#60B044,#F6C600,#FF0000" icon="https://image.freepik.com/free-icon/pneumatic-road-drill_318-34720.png" value="100" unit="°C" title="Drilling Temperature" status="stopped"></Tile>
                     </Col>
                     <Col md={3}>
-                      <Tile containerId="millingTile5" streamId="mill" icon="https://d30y9cdsu7xlg0.cloudfront.net/png/170362-200.png" value="100" unit="°C" title="Milling Temperature" status="stopped"></Tile>
+                      <Tile containerId="millingTile5" streamId="mill" borders="150,200,250,300" colors="#1e90ff,#00bfff,#60B044,#F6C600,#FF0000" icon="https://d30y9cdsu7xlg0.cloudfront.net/png/170362-200.png" value="100" unit="°C" title="Milling Temperature" status="stopped"></Tile>
                     </Col>
                     <Col md={3}>
-                      <Tile containerId="drillingTile6" streamId="drill" icon="https://image.freepik.com/free-icon/pneumatic-road-drill_318-34720.png" value="100" unit="°C" title="Drilling Temperature" status="stopped"></Tile>
+                      <Tile containerId="drillingTile6" streamId="drill" borders="150,200,250,300" colors="#1e90ff,#00bfff,#60B044,#F6C600,#FF0000" icon="https://image.freepik.com/free-icon/pneumatic-road-drill_318-34720.png" value="100" unit="°C" title="Drilling Temperature" status="stopped"></Tile>
                     </Col>
                     <Col md={3}>
-                      <Tile containerId="millingTile6" streamId="mill" icon="https://d30y9cdsu7xlg0.cloudfront.net/png/170362-200.png" value="100" unit="°C" title="Milling Temperature" status="stopped"></Tile>
+                      <Tile containerId="millingTile6" streamId="mill" borders="150,200,250,300" colors="#1e90ff,#00bfff,#60B044,#F6C600,#FF0000" icon="https://d30y9cdsu7xlg0.cloudfront.net/png/170362-200.png" value="100" unit="°C" title="Milling Temperature" status="stopped"></Tile>
                     </Col>
                   </Row>
               </Grid>
